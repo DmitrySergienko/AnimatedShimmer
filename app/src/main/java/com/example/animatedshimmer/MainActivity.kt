3aeb0c717ab8aaad6b0b15b47eb18fee442dc1dc
DmitrySergienko <dmitrisergienko@gmail.com>
2022-08-17 13:47:04 +0400

    1. ShimmerAnimation

--- app/src/main/java/com/example/animatedshimmer/MainActivity.kt
+++ app/src/main/java/com/example/animatedshimmer/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.animatedshimmer.ui.AnimatedShimmer
 import com.example.animatedshimmer.ui.theme.AnimatedShimmerTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +23,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    AnimatedShimmer()
                 }
             }
         }
@@ -34,10 +35,3 @@ fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    AnimatedShimmerTheme {
-        Greeting("Android")
-    }
-}
